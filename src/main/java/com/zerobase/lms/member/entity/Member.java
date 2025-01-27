90b6d208cf1914bc316169eab5fea0c623f84d39
Ho-JunYu <hojoon1011@gmail.com>
2025-01-28 03:33:33 +0900

    회원가입시 해당 이메일로 메일 전송 구현 완료

--- src/main/java/com/zerobase/lms/member/entity/Member.java
+++ src/main/java/com/zerobase/lms/member/entity/Member.java
@@ -18,4 +18,7 @@ public class Member {
     private String password;
     private LocalDateTime regDt;
 
+    private boolean emailAuthYn;
+    private String emailAuthKey;
+
 }
--- src/main/java/com/zerobase/lms/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/zerobase/lms/member/service/impl/MemberServiceImpl.java
@@ -1,19 +1,23 @@
-package com.zerobase.lms.member.service;
+package com.zerobase.lms.member.service.impl;
 
+import com.zerobase.lms.components.MailComponents;
 import com.zerobase.lms.member.entity.Member;
 import com.zerobase.lms.member.model.MemberInput;
 import com.zerobase.lms.member.repository.MemberRepository;
+import com.zerobase.lms.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final MailComponents mailComponents;
 
     @Override
     public boolean register(MemberInput parameter) {
@@ -23,14 +27,24 @@ public class MemberServiceImpl implements MemberService {
             return false;
         }
 
+        String uuid = UUID.randomUUID().toString();
+
         Member member = new Member();
         member.setUserId(parameter.getUserId());
         member.setUserName(parameter.getUserName());
         member.setPassword(parameter.getPassword());
         member.setPhone(parameter.getPhone());
         member.setRegDt(LocalDateTime.now());
-
+        member.setEmailAuthYn(false);
+        member.setEmailAuthKey(uuid);
         memberRepository.save(member);
+
+        String email = parameter.getUserId();
+        String subject = "lms 사이트 가입을 축하드립니다.";
+        String text = "<p>lms 사이트 가입을 축하드립니다.<p><p>아래 링크를 클릭하셔서 가입을 완료 하세요.</p>"
+                + "<div><a href='http://localhost:8080/member/email-auth?id=" + uuid + "'> 가입 완료 </a></div>";
+        mailComponents.sendMail(email, subject, text);
+
 
         return true;
     }
